fa4840b4fc1c5d2b2ccf42b49ee53214d050841a
jiin <jiin9802@naver.com>
2021-07-06 13:29:22 +0900

    Initial Commit

---
+++ homework/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homework
+//
+//  Created by 정지인님/Comm Media Cell on 2021/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ homework/homeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeworkApp.swift
+//  homework
+//
+//  Created by 정지인님/Comm Media Cell on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct homeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
