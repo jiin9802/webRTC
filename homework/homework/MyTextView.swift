4729cd31ec5eb9913c93caa77e96d846e8897450
Kim Ki Cheol <kim.kicheol@sktelecom.com>
2021-07-07 19:30:32 +0900

    reset repository

---
+++ homework/homework/MyTextView.swift
@@ -0,0 +1,16 @@
+//
+//  MyTextView.swift
+//  homework
+//
+//  Created by 정지인님/Comm Media Cell on 2021/07/06.
+//
+
+import SwiftUI
+
+struct MyTextView: View{
+    var body: some View{
+        Text("hello")
+    }
+    
+}
+
