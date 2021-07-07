82d73534f948e046796a43100a0c6a3c37377318
SKTelecom 정지인 <jiin9802@naver.com>
2021-07-07 11:35:56 +0900

    initial commit

--- homework/ContentView.swift
+++ homework/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State
+    private var isActivated:Bool=false
+    
+    @State
+    private var showDetails:Bool=false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        VStack{
+            MyHstackView()
+            MyHstackView()
+            MyHstackView()
+            
+            NavigationLink(destination: MyTextView()) { (/*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/ )}
+            Button(action: {self.showDetails.toggle()})
+            {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+            if showDetails{
+                Text("you should see me in a crown")
+                    .font(.largeTitle)
+                
+            }
+        }
+        }
     }
 }
-
+    
+struct MyHstackView:View{
+    var body: some View{
+        HStack{
+            Text("1!")
+                .padding()
+            Text("2!")
+                .padding()
+            Text("3!")
+                .padding()
+            
+        
+        }.background(Color.pink)
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+} //이걸 통해 오른쪽 화면을 볼 수 있다.
--- homework/MyTextView.swift
+++ homework/MyTextView.swift
@@ -5,4 +5,12 @@
 //  Created by 정지인님/Comm Media Cell on 2021/07/06.
 //
 
-import Foundation
+import SwiftUI
+
+struct MyTextView: View{
+    var body: some View{
+        Text("hello")
+    }
+    
+}
+
